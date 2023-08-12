9efd5af59d3109cbdeb6200fd9db801ef0ae64e2
Arun-hn <94700369+Arun-hn@users.noreply.github.com>
2023-08-12 19:59:33 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,31 +1,27 @@
 pipeline {
     agent any
-    
-    environment {
-        AWS_ACCESS_KEY_ID = credentials('aws-access-key')
-        AWS_SECRET_ACCESS_KEY = credentials('aws-secret-key')
-        AWS_REGION = 'ap-south-1'
-        S3_BUCKET = 'my-web-app-bucket'
-    }
 
     stages {
         stage('Checkout') {
             steps {
-                checkout scm
+                // Checkout your repository from GitHub
+                checkout([$class: 'GitSCM', 
+                          branches: [[name: '*/main']], 
+                          userRemoteConfigs: [[url: 'https://github.com/Arun-hn/Jenkins.git']]])
             }
         }
-        
-        stage('Build') {
+
+        stage('Build and Package') {
             steps {
-                sh 'npm install' // Assuming you need to install dependencies
+                // Build Docker image and package your code
+                sh 'docker build -t mywebapp .'
             }
         }
-        
-        stage('Deploy to S3') {
+
+        stage('Deploy') {
             steps {
-                withAWS(region: AWS_REGION, credentials: 'aws-credentials') {
-                    sh "aws s3 sync . s3://${S3_BUCKET} --delete"
-                }
+                // Deploy the Docker container
+                sh 'docker run -d -p 80:80 mywebapp'
             }
         }
     }
